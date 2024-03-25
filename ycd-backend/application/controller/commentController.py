4ba8a6bbfb0787f0364ba81e175a6ed1db335aa1
Ackermann <jann.a2@schule.bremen.de>
2024-03-25 12:17:34 +0100

    changed functions in CommentController

--- ycd-backend/application/controller/commentController.py
+++ ycd-backend/application/controller/commentController.py
@@ -1,24 +1,43 @@
-import googleapiclient.discovery
-
-
+import requests
 
 class CommentController:
-    #gets a videoId and gives back the comments of the youtbe video in json format
+    #gets a videoId and gives back the comments of the youtube video as dictionary
     def get_comments(video_id):
-        api_service_name = "youtube"
-        api_version = "v3"
-        DEVELOPER_KEY = "AIzaSyCSwEhTATLxgxwpti-WLy0-8ylt4onPizg"
+        base_url = "https://www.googleapis.com/youtube/v3/commentThreads"
+        params = {
+            'key': "AIzaSyCSwEhTATLxgxwpti-WLy0-8ylt4onPizg",
+            'videoId': video_id,
+            'part': "snippet",
+            'maxResults': 100
+        }
 
-        youtube = googleapiclient.discovery.build(
-            api_service_name, api_version, developerKey=DEVELOPER_KEY
-        )
-        request = youtube.commentThreads().list(part="snippet", videoId=video_id)
-        response = request.execute()
+        all_comments = []
 
-        return response
+        while True:
+            response = requests.get(base_url, params=params)
+            data = response.json()
+
+            all_comments.extend(data['items'])
+
+            if 'nextPageToken' in data:
+                params['pageToken'] = data['nextPageToken']
+            else:
+                break
+        return all_comments
+    
+    #gets full comment dictionaary and clean it to have only the commentsTexts
+    def clean_comments(comments:dict):
+        key = "snippet"
+        subkey1 = "topLevelComment"
+        subkey2 = "snippet"
+        final_key = "textOriginal"
+        clean_comments = [item.get(key, {}).get(subkey1, []).get(subkey2, {}).get(final_key) for item in comments]
+        return clean_comments
     
     def controller(video_id):
-        comments_json = CommentController.get_comments(video_id)
+        comments = CommentController.get_comments(video_id)
+        print(CommentController.clean_comments(comments))
 
 
-print(CommentController.controller("Yxt_ieWY4LM"))
+# zum Testen: Video = https://www.youtube.com/watch?v=Yxt_ieWY4LM
+CommentController.controller("Yxt_ieWY4LM")
